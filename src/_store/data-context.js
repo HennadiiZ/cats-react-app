3e5dd1fe684e4cccceec5e2cce02e2bcafb9d084
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-04-15 21:12:14 -0600

    instaling ts

--- src/_store/data-context.js
+++ src/_store/data-context.js
@@ -52,3 +52,76 @@ export const fetchCatsList = () => async (dispatch) => {
     dispatch(catsSlice.actions.setIsLoading(false));
   }
 };
+
+
+
+
+
+//tsx
+// import { createSlice, configureStore, PayloadAction } from '@reduxjs/toolkit';
+// import axios from 'axios';
+// import { THE_CAT_API } from '../constants/constants';
+
+// // ------
+// interface Cat {
+//   id: string;
+//   url: string;
+// }
+
+// interface CatsState {
+//   cats: Cat[];
+//   isLoading: boolean;
+// }
+
+// //{ RootState, AppDispatch }
+// export interface RootState {
+//   cats: CatsState;
+// }
+
+// export type AppDispatch = typeof store.dispatch;
+// //-----------
+
+// const initialState: CatsState = {
+//   cats: [],
+//   isLoading: false,
+// };
+
+// export const catsSlice = createSlice({
+//   name: 'cats',
+//   initialState: initialState,
+//   reducers: {
+//     setCats: (state, action: PayloadAction<Cat[]>) => {
+//       state.cats = action.payload;
+//     },
+//     setIsLoading: (state, action: PayloadAction<boolean>) => {
+//       state.isLoading = action.payload;
+//     },
+//   },
+// });
+
+// export const catsActions = catsSlice.actions;
+
+// const store = configureStore({
+//   reducer: catsSlice.reducer,
+// });
+
+// export default store;
+
+// export const fetchCatsList = () => async (dispatch: any) => {
+//   dispatch(catsSlice.actions.setIsLoading(true));
+
+//   try {
+//     const response = await axios.get<any>(THE_CAT_API);
+
+//     if (response.status !== 200) {
+//       throw new Error('Network response was not ok');
+//     }
+
+//     dispatch(catsSlice.actions.setCats(response.data));
+//   } catch (error) {
+//     console.error('There was a problem with the axios operation:', error);
+//   } finally {
+//     dispatch(catsSlice.actions.setIsLoading(false));
+//   }
+// };
+
--- src/index.js
+++ src/index.js
@@ -15,3 +15,25 @@ root.render(
   </Provider>
 );
 reportWebVitals();
+
+
+
+//tsx
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// import { Provider } from 'react-redux';
+// import store from './_store/data-context';
+
+// const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+// root.render(
+//   <Provider store={store}>
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>
+//   </Provider>
+// );
+// reportWebVitals();
